13345aa614835cf69f77567eb5b391b1f452df9c
Kartik Hegadi <67619115+kartikhegadi@users.noreply.github.com>
2023-01-07 01:25:16 +0530

    Add files via upload

---
+++ prog_7/src/prog_7/CRC1.java
@@ -0,0 +1,111 @@
+package prog_7;
+
+import java.util.Scanner;
+import java.io.*;
+public class CRC1 {
+
+    public static void main(String args[]) {
+
+    Scanner sc = new Scanner(System.in);
+
+    //Input Data Stream
+    System.out.print("Enter message bits: "); // user's string to encrypt
+    String message = sc.nextLine();
+    
+    System.out.print("Enter generator: ");   // generator is pre-defined value
+    String generator = sc.nextLine();
+      
+    int data[] = new int[message.length() + generator.length() - 1]; //bits = DATA + (N-1)ZEROS
+    int divisor[] = new int[generator.length()]; // is same size of generator
+    
+    for(int i=0;i<message.length();i++)
+    {
+    data[i] = Integer.parseInt(message.charAt(i)+"");
+    }
+    
+    for(int i=0;i<generator.length();i++)
+    {
+    divisor[i] = Integer.parseInt(generator.charAt(i)+"");
+    }
+
+//Calculation of CRC
+    for(int i=0;i<message.length();i++)
+    {
+    	if(data[i]==1)             // if 1 take action  // if 0 don't take action
+    	{
+        	for(int j=0;j<divisor.length;j++) // do operation till divisor length
+        	{
+            data[i+j] ^= divisor[j];    // '^' = BITWISE XOR  // storing data into data it self
+        	} 								// a=a^b
+    	}
+	}
+
+//Display CRC
+    System.out.print("The checksum code is: "); // it will only print the data it self eg:000000000001110
+    for(int i=0;i<message.length();i++)
+    {
+    	data[i] = Integer.parseInt(message.charAt(i)+"");  // the original data will be copied
+    }
+    
+    for(int i=0;i<data.length;i++) 
+    {
+    System.out.print(data[i]);       // just printing the data after appending the reminder
+    }
+    System.out.println();
+    
+    
+
+//Check for input CRC code
+    System.out.print("Enter checksum code: ");
+    {
+    	message = sc.nextLine();
+    }
+    
+    System.out.print("Enter generator: ");
+    {
+    generator = sc.nextLine();
+    }	
+    
+    data = new int[message.length() + generator.length() - 1];
+    divisor = new int[generator.length()];
+    
+    for(int i=0;i<message.length();i++)
+    {
+    data[i] = Integer.parseInt(message.charAt(i)+"");
+    }
+    for(int i=0;i<generator.length();i++)
+    {
+    divisor[i] = Integer.parseInt(generator.charAt(i)+"");
+    }
+
+    //Calculation of remainder
+    for(int i=0;i<message.length();i++) 
+    {
+    if(data[i]==1)
+    {	
+    	for(int j=0;j<divisor.length;j++)
+    	{
+            data[i+j] ^= divisor[j];
+    	}
+    }
+    }
+
+//Display validity of data
+boolean valid = true;
+for(int i=0;i<data.length;i++)
+{
+    if(data[i]==1)
+    {
+        valid = false;
+        break;
+        
+    }
+}
+
+if(valid==true) 
+    System.out.println("Data stream is valid");
+else 
+    System.out.println("Data stream is invalid. CRC error occurred.");
+}
+
+}
